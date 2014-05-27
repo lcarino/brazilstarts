f9f59461b3381d78481ca23777787773f7d81272
memocostecho <memogrr.dohko@gmail.com>
2014-05-27 11:03:22 -0500

    imagenes a la shit

--- AlbumBrazil/src/com/example/albumbrazil/SplashScreen.java
+++ AlbumBrazil/src/com/example/albumbrazil/SplashScreen.java
@@ -1,15 +1,10 @@
 package com.example.albumbrazil;
 
-import com.example.albumbrazil.util.SystemUiHider;
 
-import android.annotation.TargetApi;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
